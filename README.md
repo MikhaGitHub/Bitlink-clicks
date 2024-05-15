92cca85384bd262263b808be9bcfbf43f4cc07a2
MikhaGitHub <157389226+MikhaGitHub@users.noreply.github.com>
2024-05-15 20:26:36 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Bitlink-clicks
+# Reduction url.
+
+## Description:
+This project work with Bitly.com for reduction url and check.
+
+## Step one is modules and libriaries:
+Install requirements.txt
+Command is ```pip install -r requirements.txt```
+
+## Also needed download zip archive and unpack it
+
+## In this project used TOKEN Bitly.com, add your TOKEN in env file
+
+
+# How run the code in command prompt
+1. Open cmd press on keyboark Win + R and write cmd and press enter.
+2. Go to your directory where located your main.py.
+3. Run code a command in prompt python main.py "and your argument as a link".
+
+# For check transition on bitlink
+1. Run code a command in prompt python main.py "and your argument as a bitlink".
+
---
+++ main.py
@@ -0,0 +1,55 @@
+import requests
+from urllib.parse import urlparse
+import os
+from dotenv import load_dotenv
+
+
+def shorten_link(token, url):
+    bitlink_url = 'https://api-ssl.bitly.com/v4/shorten'
+    headers = {'Authorization': f'Bearer {token}'}
+    body = {'long_url': url}
+
+    response = requests.post(bitlink_url, headers=headers, json=body)
+    response.raise_for_status()
+    return response.json()['id']
+
+
+def get_clicks(bitlink, token):
+    sample_url = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'
+    headers = {'Authorization': f'Bearer {token}'}
+
+    params = {"units": -1, "unit": "month"}
+
+    response = requests.get(sample_url, headers=headers, params=params)
+    response.raise_for_status()
+    return response.json()['total_clicks']
+
+
+def is_bitlink(url, token):
+    parsed_url = urlparse(url)
+    bitlink = f'{parsed_url.netloc}{parsed_url.path}'
+    check_bitlink = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}'
+    headers = {'Authorization': f'Bearer {token}'}
+    response = requests.get(check_bitlink, headers=headers)
+    return response.ok
+
+
+def main():
+    load_dotenv()
+    token = os.getenv('BITLY_TOKEN')
+    url = input('Enter url: ')
+    try:
+        response = is_bitlink(url,token)
+        if response:
+            parsed_url = urlparse(url)
+            bitlink = f'{parsed_url.netloc}{parsed_url.path}'
+            print(f'Total clicks: {get_clicks(bitlink, token)}')
+        else:
+            print(f'Bitlink: {shorten_link(token, url)}')
+    except requests.exceptions.HTTPError as error:
+        print(f'Error: {error}')
+
+
+
+if __name__ == '__main__':
+    main()
